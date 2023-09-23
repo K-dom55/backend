ada7f35296f7e89dc59a1c0bb73ebed82dd16600
shotny <yeahh2483@gmail.com>
2023-09-23 19:39:59 +0900

    feat: search article list

--- src/main/java/com/kdom/backend/repository/MemberRepository.java
+++ src/main/java/com/kdom/backend/repository/MemberRepository.java
@@ -5,9 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, Long>{
-
+public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByIp(String ip);
-
     boolean existsByIp(String ip);
+
 }
